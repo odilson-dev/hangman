5a683b78cd20416887868a6f8fd485e937aec0f1
Italis Odilson Woodly <italisodilsonwoodly@hotmail.com>
2023-09-28 13:28:04 -0400

    Implement the find_secret_word method
    When a new game is started, my script load in the dictionary and
    randomly select a word between 5 and 12 characters long for the
    secret word.

--- main.rb
+++ main.rb
@@ -3,6 +3,10 @@ require 'open-uri'
 
 url = URI.open('https://raw.githubusercontent.com/first20hours/google-10000-english/master/google-10000-english-no-swears.txt')
 
+def find_secret_word(dictionnary)
+    dictionnary.readlines.map{ |word| word.chomp("\n") if (5..12).to_a.include? word.chomp("\n").length }.compact.sample
+ end
+
 dirname = "dictionnary"
 Dir.mkdir(dirname) unless File.exist? dirname
 
@@ -13,7 +17,6 @@ dictionnary = File.open("#{dirname}/dictionnary.txt")
 
 
 
-puts dictionnary.read
-
+p find_secret_word(dictionnary)
 dictionnary.close
 
